11dcc31dd23d361532b178a3057afc8f468c1cc2
TomasLeTrain <tomylopezd@gmail.com>
2025-09-18 10:46:45 -0400

    added weekly contest 1 solutions and problem data

---
+++ past_contests/lesson2_contest/addiply_multiple.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+	int a, b; cin >> a >> b;
+	cout << a + b << " " << a * b << endl;
+}
+
+int main(){
+	int tc = 1;
+	cin >> tc;
+	while(tc--){
+		solve();
+	}
+}
---
+++ past_contests/lesson2_contest/minimize.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+	int a, b; cin >> a >> b;
+	cout << b-a << endl;
+}
+
+int main(){
+	int tc; cin >> tc;
+	while(tc--) solve();
+}
